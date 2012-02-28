0b8c881be9e6ba7584584d79285bacfee5faa251
Dirlei Dionisio <dirlei@gmail.com>
2012-02-28 15:44:29 -0300

    adding dependencies and test code

--- Resources/app.js
+++ Resources/app.js
@@ -1,1 +1,149 @@
-Ti.API.info('Hello!');
+/*globals TitaniumOAuth*/
+
+// includes
+Ti.include('lib/titanium_oauth.js');
+
+// ui *******************************************
+var tabGroup = Ti.UI.createTabGroup();
+
+// setup tab ------------------------------------
+var winSetup = Ti.UI.createWindow({
+	title: 'setup',
+	backgroundColor: 'black'
+});
+
+var textFieldTop=5;
+
+var addTextField = function(window, propertyName) {
+	var textField = Ti.UI.createTextField({
+		hintText: propertyName,
+		value: Ti.App.Properties.getString(propertyName),
+		top: '' + textFieldTop + 'dp',
+		height: '30dp',
+		left: '5dp',
+		right: '5dp',
+		backgroundColor: 'white',
+		autocapitalization: false
+	});
+	
+	textField.addEventListener('change', function(e) {
+		Ti.App.Properties.setString(propertyName, e.value);
+	});
+	
+	window.add(textField);
+	textFieldTop += 35;
+};
+
+addTextField(winSetup, 'consumer_key');
+addTextField(winSetup, 'consumer_secret');
+addTextField(winSetup, 'request_token_url');
+addTextField(winSetup, 'authorize_url');
+addTextField(winSetup, 'access_token_url');
+
+tabGroup.addTab(Ti.UI.createTab({
+	title: winSetup.title,
+	window: winSetup
+}));
+
+// test tab --------------------------------------
+var winTest = Ti.UI.createWindow({
+	title: 'test',
+	backgroundColor: 'black'
+});
+
+textFieldTop=5;
+
+addTextField(winTest, 'method');
+addTextField(winTest, 'action');
+addTextField(winTest, 'parameter_name_1');
+addTextField(winTest, 'parameter_value_1');
+addTextField(winTest, 'parameter_name_2');
+addTextField(winTest, 'parameter_value_2');
+addTextField(winTest, 'parameter_name_3');
+addTextField(winTest, 'parameter_value_3');
+
+var b1 = Ti.UI.createButton({
+	bottom: 10,
+	left: 10,
+	right: 10,
+	height: 50,
+	title: 'test'
+});
+
+winTest.add(b1);
+
+tabGroup.addTab(Ti.UI.createTab({
+	title: winTest.title,
+	window: winTest
+}));
+
+// events
+b1.addEventListener('click', function(){
+	var parameters = {
+		consumerKey: Ti.App.Properties.getString('consumer_key'),
+		consumerSecret: Ti.App.Properties.getString('consumer_secret'),
+		requestTokenURL: Ti.App.Properties.getString('request_token_url'),
+		userAuthorizationURL: Ti.App.Properties.getString('authorize_url'),
+		accessTokenURL: Ti.App.Properties.getString('access_token_url')
+	};
+	
+	if (parameters.consumerKey && 
+		parameters.consumerSecret &&
+		parameters.requestTokenURL &&
+		parameters.userAuthorizationURL &&
+		parameters.accessTokenURL) {
+			
+		var options = {
+			method: Ti.App.Properties.getString('method'),
+			action: Ti.App.Properties.getString('action'),
+			parameters: [], // will be setted later
+			
+			parameter_name_1: Ti.App.Properties.getString('parameter_name_1'),
+			parameter_value_1: Ti.App.Properties.getString('parameter_value_1'),
+			parameter_name_2: Ti.App.Properties.getString('parameter_name_2'),
+			parameter_value_2: Ti.App.Properties.getString('parameter_value_2'),
+			parameter_name_3: Ti.App.Properties.getString('parameter_name_3'),
+			parameter_value_3: Ti.App.Properties.getString('parameter_value_3')
+		};
+
+		if (options.method && 
+			options.action &&
+			options.parameter_name_1 &&
+			options.parameter_value_1) {
+				
+			var aAux=[];
+			aAux.push(options.parameter_name_1);
+			aAux.push(options.parameter_value_1);
+			options.parameters.push(aAux);
+			
+			if (options.parameter_name_2 && options.parameter_value_2) {
+				aAux.push(options.parameter_name_2);
+				aAux.push(options.parameter_value_2);
+				options.parameters.push(aAux);
+			}
+ 	
+			if (options.parameter_name_3 && options.parameter_value_3) {
+				aAux.push(options.parameter_name_3);
+				aAux.push(options.parameter_value_3);
+				options.parameters.push(aAux);
+			}
+ 	
+			var oauth = new TitaniumOAuth(parameters);
+		
+			oauth.requestToken(function() {
+			    oauth.request(options, function(data) {
+			        Ti.API.info('#response is: ' + data);
+			        alert('#response is: ' + data);
+			    });
+			});
+		} else {
+			alert('Did you forget the test params?');
+		}
+	} else {
+		alert('Did you forget the setup?');
+	}
+});
+
+
+// starting app
+tabGroup.open();
---
+++ Resources/lib/titanium_oauth.js
@@ -0,0 +1,375 @@
+/*
+* Titanium OAuth Client
+*
+* Copyright 2010, Social Vitamin, Inc.
+* Licensed under the MIT
+* Copyright (c) 2010 Social Vitamin, Inc.
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+Ti.include('sha1.js');
+Ti.include('oauth.js');
+
+var TitaniumOAuth = function(params) {
+	// params:
+	// consumerKey
+	// consumerSecret
+	// signatureMethod (optional)
+	// requestTokenURL
+	// userAuthorizationURL
+	// accessTokenURL
+	// oauthVersion (optional)
+
+	var self = this;
+	//var currentWin = Ti.UI.currentWindow; -> unused
+	var authWebView = null;
+	var oauthWin = null;
+
+	var consumer = {
+	    consumerKey:      params.consumerKey,
+	    consumerSecret:   params.consumerSecret,
+	    serviceProvider: {
+	        signatureMethod:     params.signatureMethod || 'HMAC-SHA1',
+	        requestTokenURL:     params.requestTokenURL,
+	        userAuthorizationURL:params.userAuthorizationURL,
+	        accessTokenURL:      params.accessTokenURL,
+			oauthVersion:        params.oauthVersion || '1.0'
+	    }
+	};
+	
+	var accessor = {
+	    consumerSecret: consumer.consumerSecret,
+	    tokenSecret: ''
+	};
+
+	this.loggedIn = function() {
+		return  (Ti.App.Properties.getString('accessToken') == null && Ti.App.Properties.getString('accessTokenSecret') == null) ? false : true;
+	};
+	
+	// Get Authorization PIN
+	var getPIN = function(e)
+	{
+		var html = authWebView.evalJS("document.getElementById('oauth_pin').innerHTML");
+		if (html != '') {
+			var regex = new RegExp("([0-9]+)", "m"); 
+			if (regex) {
+				var pin = html.match(regex)[0]; 
+				if (pin) {
+					self.accessToken(pin);
+					if(oauthWin != null) {
+						oauthWin.close();
+					}				
+				}
+			}
+		}
+	};
+	
+	// Request Token
+	this.requestToken = function(callback){
+		
+		if (Ti.App.Properties.getString('accessToken') != null &&
+		Ti.App.Properties.getString('accessTokenSecret') != null) {
+
+			// Login
+			self.dispatch('login');
+
+			callback();
+
+			return;
+
+		}
+		
+		var message = {
+		    method: 'GET',
+		    action: consumer.serviceProvider.requestTokenURL,
+		    parameters: [
+		       ['oauth_signature_method', consumer.serviceProvider.signatureMethod],
+		       ['oauth_consumer_key', consumer.consumerKey],
+		       ['oauth_version', consumer.serviceProvider.oauthVersion]
+		   ]
+		};
+		
+		OAuth.setTimestampAndNonce(message);
+		OAuth.setParameter(message, "oauth_timestamp", OAuth.timestamp());
+		OAuth.SignatureMethod.sign(message, accessor);
+		
+		var finalUrl = OAuth.addToURL(message.action, message.parameters);
+
+		var xhr = Titanium.Network.createHTTPClient();
+		xhr.onload = function()
+		{
+			
+			if (!this.responseText.match(/oauth_token=([^&]+)&/)){
+				self.logout();
+			}
+			
+			// Set Tokens
+			Ti.App.Properties.setString('oauthToken', this.responseText.match(/oauth_token=([^&]+)/)[1]);
+			Ti.App.Properties.setString('oauthTokenSecret', this.responseText.match(/oauth_token_secret=([^&]+)/)[1]);
+			
+			// Access Token Secret
+			accessor.tokenSecret = Ti.App.Properties.getString('accessTokenSecret');
+			
+			// Verify if we have an access token if we dont show auth webview
+			if (Ti.App.Properties.getString('accessToken') == null && 
+					Ti.App.Properties.getString('accessTokenSecret') == null) {
+				self.oauthWebView({
+					url: consumer.serviceProvider.userAuthorizationURL + '?' + this.responseText
+				});
+			} else {
+				callback();
+			}
+			
+		};
+		xhr.onerror = function(e) {
+			Ti.UI.createAlertDialog({
+                title: 'Service Unavailable',
+                message: 'Service unavailable please try again later.'
+            }).show();
+			
+			// Logout
+			self.logout();
+		};
+		xhr.open('GET', finalUrl);
+		xhr.send();
+	
+	};
+	
+	// Access Token
+	this.accessToken = function(pin, callback){
+		
+	    var message = {
+	        method: 'GET',
+	        action: consumer.serviceProvider.accessTokenURL,
+		   parameters: [
+	           ['oauth_signature_method', consumer.serviceProvider.signatureMethod],
+	           ['oauth_consumer_key', consumer.consumerKey],
+	           ['oauth_version', consumer.serviceProvider.oauthVersion],
+	           ['oauth_token', Ti.App.Properties.getString('oauthToken')],
+	           ['oauth_verifier', pin]
+	       ]
+	    };
+
+	    OAuth.setTimestampAndNonce(message);
+	    OAuth.setParameter(message, "oauth_timestamp", OAuth.timestamp());
+	    OAuth.SignatureMethod.sign(message, accessor);
+		
+	    var finalUrl = OAuth.addToURL(message.action, message.parameters);
+
+		var xhr = Titanium.Network.createHTTPClient();
+		xhr.onload = function()
+		{
+			
+			if (!this.responseText.match(/oauth_token=([^&]+)&/)){
+				self.logout();
+			}
+			
+			Ti.App.Properties.setString('accessToken', this.responseText.match(/oauth_token=([^&]+)&/)[1]);
+			Ti.App.Properties.setString('accessTokenSecret', this.responseText.match(/oauth_token_secret=([^&]+)&/)[1]);	
+
+			// Login
+			self.dispatch('login');
+		};
+		xhr.onerror = function(e) {
+
+			Ti.UI.createAlertDialog({
+                title: 'Service Unavailable',
+                message: 'Service unavailable please try again later.'
+            }).show();
+			
+			// Logout
+			self.logout();
+		};
+		xhr.open('GET', finalUrl);
+		xhr.send();
+
+	};
+	
+	// Show Authorization Web View
+	this.oauthWebView = function(params)
+	{
+		
+		var t = Titanium.UI.create2DMatrix().scale(0);
+		
+		var win = Ti.UI.createWindow({
+			backgroundColor:'#CCE6F0',
+			borderWidth:4,
+			borderColor:'#52D3FE',
+			height:350,
+			width:275,
+			borderRadius:10,
+			transform:t
+        });
+
+		// WebView
+	    authWebView = Ti.UI.createWebView({
+				url: params.url
+			});
+		
+		authWebView.addEventListener('load', getPIN);
+	    win.add(authWebView);
+		
+		// Remove window button
+		var cl = Ti.UI.createLabel({
+			width: 24,
+			height: 24,
+			right: 10, 
+			top: 10,
+			borderRadius: 6,
+			borderColor: '#52D3FE',
+			backgroundColor: '#52D3FE',
+			text: 'X',
+			textAlign: 'center',
+			font:{fontSize:11, fontWeight: 'bold'},
+			color:'#fff'
+		});
+		
+		cl.addEventListener('click', function(e){
+			var a = Titanium.UI.createAnimation({opacity: 0, duration: 300});
+			win.animate(a, function(){
+				win.close();
+			});
+		});
+		
+		win.add(cl);
+		
+		win.open();
+		
+		// Window Animation
+		var t1 = Ti.UI.create2DMatrix().scale(0);
+		var t2 = Ti.UI.create2DMatrix().scale(1.1);
+		var t3 = Ti.UI.create2DMatrix().scale(1.0);
+		
+		var a = Titanium.UI.createAnimation({transform: t1, duration: 300});
+		var a2 = Titanium.UI.createAnimation({transform: t2, duration: 350});
+		var a3 = Titanium.UI.createAnimation({transform: t3, duration: 400});
+		
+		win.animate(a);
+		
+		a.addEventListener('complete', function()
+		{
+			win.animate(a2);
+		});
+		
+		a2.addEventListener('complete', function()
+		{
+			win.animate(a3);
+		});
+		
+		// Set the window so we can remove it in the callback
+		oauthWin = win;
+	
+	};
+	
+	// Request
+	this.request = function(options, callback) {
+		 
+        var message = {
+			method: options.method,
+			action: options.action,
+			parameters: [
+			['oauth_signature_method', consumer.serviceProvider.signatureMethod],
+			['oauth_consumer_key', consumer.consumerKey],
+			['oauth_version', consumer.serviceProvider.oauthVersion],
+			['oauth_token', Ti.App.Properties.getString('accessToken')]
+			]
+		};
+
+		for (param in options.parameters) {
+			message.parameters.push(options.parameters[param]);
+		};
+
+		// Access Token Secret
+		accessor.tokenSecret = Ti.App.Properties.getString('accessTokenSecret');
+
+		OAuth.setTimestampAndNonce(message);
+		OAuth.SignatureMethod.sign(message, accessor);
+
+		var finalUrl = OAuth.addToURL(message.action, message.parameters);
+
+		var xhr = Titanium.Network.createHTTPClient({
+			timeout: 200000
+		});
+		xhr.onload = function() {
+			callback(this.responseText);
+		};
+		xhr.onerror = function(e) {
+
+			Ti.UI.createAlertDialog({
+				title: 'Service Unavailable',
+				message: 'An error ocurred while making a request.'
+			}).show();
+
+			// Logout
+			self.dispatch('logout');
+		};
+		xhr.open(options.method, finalUrl, false);
+		xhr.send();
+		
+	};
+	
+	this.logout = function() {
+		Ti.App.Properties.setString('oauthToken', null);
+	    Ti.App.Properties.setString('oauthTokenSecret', null);
+		Ti.App.Properties.setString('accessToken', null);
+	    Ti.App.Properties.setString('accessTokenSecret', null);
+
+		// Logout
+		self.dispatch('logout');
+	};
+
+};
+
+// Dispatcher
+function Dispatcher() {
+	this.events = [];
+};
+
+Dispatcher.prototype.addEventListener = function(event,callback){
+	this.events[event] = this.events[event] || [];
+	if ( this.events[event] ) {
+		this.events[event].push(callback);
+	}
+};
+
+Dispatcher.prototype.removeEventListener = function(event,callback){
+	if ( this.events[event] ) {
+		var listeners = this.events[event];
+		for ( var i = listeners.length-1; i>=0; --i ){
+			if ( listeners[i] === callback ) {
+				listeners.splice( i, 1 );
+				return true;
+			}
+		}
+	}
+	return false;
+};
+
+Dispatcher.prototype.dispatch = function(event){
+	if ( this.events[event] ) {
+		var listeners = this.events[event], len = listeners.length;
+		while ( len-- ) {
+			listeners[len](this);
+		}		
+	}
+};
+
+TitaniumOAuth.prototype = new Dispatcher();
